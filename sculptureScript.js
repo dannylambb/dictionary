5160f76d7cd8dabb1c46f941ffc809b9acc49720
dannylambb <113957487+dannylambb@users.noreply.github.com>
2025-11-25 23:34:29 -0800

    Update sculptureScript.js

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,1 +1,17 @@
+const { sculptureList } = require('./data.js');
 
+// array
+const sculptureListLengths = [];
+
+sculptureList.forEach((sculpture) => {
+  const lengthObject = {};
+
+  // loop
+  for (let key in sculpture) {
+    lengthObject[key] = sculpture[key].length; 
+  }
+
+  sculptureListLengths.push(lengthObject);
+});
+
+console.log(sculptureListLengths);
